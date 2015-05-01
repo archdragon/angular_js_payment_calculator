4423e8c3cb9226a4334b2936b973e39c2f0533e2
Przemek <przemek@krowinski.eu>
2015-05-01 19:33:47 +0200

    Add automatic rate calculator

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,12 +12,20 @@ angular.module('payCalculatorApp')
     var rent = {name: 'Rent', value: '2000'};
     var zus = {name: 'ZUS', value: '1200'};
 
+    $scope.settings = {
+      workDaysInMonth: 20.0,
+      workHoursInDay: 8.0
+    }
     $scope.expenses = [rent, zus];
     $scope.rate = {
-      minimal: { yearly: 0, monthly: 0, daily: 0 }
+      minimal: { yearly: 0,
+                 monthly: 0,
+                 daily: 0,
+                 hourly: 0 }
     };
 
     $scope.$watchCollection('expenses', function(newValue, oldValue) {
+      updateRates();
     });
 
     $scope.addExpense = function () {
@@ -26,9 +34,27 @@ angular.module('payCalculatorApp')
     };
 
     $scope.removeExpense = function (index) {
-        $scope.expenses.splice(index, 1);
+      $scope.expenses.splice(index, 1);
     };
 
     function updateRates() {
+      var workDaysInMonth = 20.0;
+      var workHoursInDay = 8.0;
+      var minimal = $scope.rate.minimal;
+      minimal.monthly = totalExpenses();
+      minimal.yearly = totalExpenses() * 12.0;
+      minimal.daily = minimal.monthly / workDaysInMonth;
+      minimal.hourly = minimal.daily / workHoursInDay;
+    }
+
+    function totalExpenses() {
+      var aLength = $scope.expenses.length;
+      var sum = 0;
+      for(var i=0; i<aLength; i++) {
+        if($scope.expenses[i] !== undefined) {
+          sum += parseFloat($scope.expenses[i].value);
+        }
+      }
+      return sum;
     }
   });
