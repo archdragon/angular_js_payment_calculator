96a1fa8336c25c8dbfae8245696b0cdf895bd418
Przemek <przemek@krowinski.eu>
2015-05-01 18:41:45 +0200

    Add expense values and minimal rates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,18 +9,26 @@
  */
 angular.module('payCalculatorApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.expenses = [
-      'Flat and office rent',
-      'Flat and office bills',
-      'ZUS',
-      'Servers',
-      'Health and medicine'
-    ];
+    var rent = {name: 'Rent', value: '2000'};
+    var zus = {name: 'ZUS', value: '1200'};
+
+    $scope.expenses = [rent, zus];
+    $scope.rate = {
+      minimal: { yearly: 0, monthly: 0, daily: 0 }
+    };
+
+    $scope.$watchCollection('expenses', function(newValue, oldValue) {
+    });
+
     $scope.addExpense = function () {
       $scope.expenses.push($scope.expense);
       $scope.expense = '';
     };
+
     $scope.removeExpense = function (index) {
         $scope.expenses.splice(index, 1);
     };
+
+    function updateRates() {
+    }
   });
